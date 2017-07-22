3d4813db719255b67b9c61b91218ae0f3310dd8f
spiceboy <spice_boy@126.com>
2017-07-22 10:58:27 +0800

    4.0

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -3,7 +3,7 @@ import reducer from './reducer.js';
 
 var state={
     title: '卖座电影',
-    barArr: [ {path:'/',txt:'首页',title: '卖座电影'}, {path:'/movie/now',txt:'影片',title: '全部影片'},{path:'/cinema',txt:'影院',title: '全部影院'} ,{path:'/',txt:'商城',title: '卖座商城'} ,{path:'/',txt:'演出',title: '全部演'} ,{path:'/login',txt:'我的',title: '登录'}, {path:'/',txt:'卖座卡',title: '卖座卡'} ]
+    barArr: [ {path:'/',txt:'首页',title: '卖座电影'}, {path:'/movie/now',txt:'影片',title: '全部电影'},{path:'/cinema',txt:'影院',title: '全部影院'} ,{path:'/',txt:'商城',title: '卖座商城'} ,{path:'/',txt:'演出',title: '全部演'} ,{path:'/login',txt:'我的',title: '登录'}, {path:'/',txt:'卖座卡',title: '卖座卡'} ]
     
 };
 
